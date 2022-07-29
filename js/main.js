69ec11c6aec9d5506fcaa312931708585fc54f15
NasimbayevFarhod <105714502+NasimbayevFarhod@users.noreply.github.com>
2022-07-29 12:14:59 +0500

    finished

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+var elModalOpenButton = document.querySelector(".btn-non");
+var elModal = document.querySelector(".site-header__nav");
+var elBody = document.querySelector("body");
 
 
-
-
-
+elModalOpenButton.addEventListener("click", function(){
+  elModal.classList.toggle("sitenav__open");
+})
 
 
 
